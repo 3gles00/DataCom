34abb25526247054ae4323c38d4f1be3838eed6f
3gles00 <haron.obaid66@gmail.com>
2023-09-28 16:43:23 +0200

    debbugging Lab2 P1 Activity 1

--- Lab2/P1/multihop/txc1.cc
+++ Lab2/P1/multihop/txc1.cc
@@ -5,12 +5,10 @@ using namespace omnetpp;
 
 class Txc1 : public cSimpleModule{
     private:
-        cMessage *event = nullptr;
-        long numSent;
-        long numReceived;
         long msgCounter;
-        simsignal_t transmissionSignal;
-        simsignal_t receptionSignal;
+        // cMessage *event;
+    public:
+        virtual ~Txc1();
     protected:
         virtual void initialize();
         virtual void handleMessage(cMessage *msg);
@@ -19,61 +17,49 @@ class Txc1 : public cSimpleModule{
 
 Define_Module(Txc1);
 
-//Txc1::~Txc1(){
-    //cancelAndDelete(msg);
-//}
+Txc1::~Txc1(){
+    // cancelAndDelete(msg);
+    // delete msgCounter;
+}
 
 void Txc1::initialize(){
-    msgCounter = 0;
-    numSent = 0;
-    numReceived = 0;
-
-    WATCH(numSent);
-    WATCH(numReceived);
-
-    transmissionSignal = registerSignal("transmissionSignal");
-    receptionSignal = registerSignal("receptionSignal");
-
-    //Start messaging if I am the first node
-    if(getIndex() == 0){
-        // msgCounter++;
-        EV << "Scheduling first send to a random time\n";
-        char msgname[20];
-        sprintf(msgname, "DATA-%ld", msgCounter);
-        cMessage *msg = new cMessage(msgname);
-        scheduleAt(par("delayTime"), msg);
-    }
+  msgCounter = 0;
+  // Start messaging if I an the first node
+  if(getIndex() == 0){
+    msgCounter++;
+    EV << "Scheduling first sent to a random time\n";
+    char msgname[20];
+    sprintf(msgname, "DATA-%ld", msgCounter);
+    cMessage *msg = new cMessage(msgname);
+    scheduleAt(par("delayTime"), msg);
+  }
 }
 
 void Txc1::handleMessage(cMessage *msg){
-   if(getIndex() == 5){
-        //Message arrived
-        EV << "Message " << msg << " arrived.\n";
+    if(getIndex() == 5){
+        // Message arrived
+        EV << "Message " << msg << " arrived\n";
+        cancelEvent(msg);
+        delete msg;
+        // Planning new message
         msgCounter++;
-        numReceived++;
-        emit(receptionSignal, numReceived);
+        char msgname[20];
+        sprintf(msgname, "DATA-%ld", msgCounter);
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(par("delayTime") + simTime() + 1.0, msg);
     }
     else{
-        //Message has to be forwarded
-        msgCounter++;
-        numReceived++;
-        emit(receptionSignal, numReceived);
-        
+        // Message has to be forwarded
         forwardMessage(msg);
     }
 }
 
 void Txc1::forwardMessage(cMessage *msg){
-    /*For this example, we always receive in the gate with
-    a lower number out of the two we have. So we forward
-    using our higher-numberd gate*/
+    // For this example, we always reveive in the gate with
+    // a lower number out of the two we have, So we forward 
+    // using our higher-numbered gate
     int n = gateSize("gate");
-    int k = n-1;
-    EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
-    
-    msgCounter++;
-    numSent++;
-    emit(transmissionSignal, numSent);
-
+    int k = n - 1;
+    EV << "Forwarding message " << msg << " on gate " << k << "\n";
     send(msg, "gate$o", k);
 }
