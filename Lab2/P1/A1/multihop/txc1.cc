5def4694eae34f843af5e4452a37029e79d11f18
3gles00 <haron.obaid66@gmail.com>
2023-09-29 17:59:51 +0200

    debugging Lab2 A1 vector recordings,

---
+++ Lab2/P1/A1/multihop/txc1.cc
@@ -0,0 +1,91 @@
+#include<cstring>
+#include<omnetpp.h>
+
+using namespace omnetpp;
+
+class Txc1 : public cSimpleModule{
+    private:
+        // long msgCounter;
+        long numSent;
+        long numReceived;
+        simsignal_t transmissionSignal;
+        simsignal_t receptionSignal;
+    public:
+        virtual ~Txc1();
+    protected:
+        virtual void initialize();
+        virtual void handleMessage(cMessage *msg);
+        virtual void forwardMessage(cMessage *msg);
+};
+
+Define_Module(Txc1);
+
+Txc1::~Txc1(){
+
+}
+
+void Txc1::initialize(){
+    // msgCounter = 0;
+    numSent = 0;
+    numReceived = 0;
+    // WATCH(msgCounter);
+    WATCH(numSent);
+    WATCH(numReceived);
+    transmissionSignal = registerSignal("transmissionSignal");
+    receptionSignal = registerSignal("receptionSignal");
+
+    // Start messaging if I an the first node
+    if(getIndex() == 0){
+        numSent++;
+        EV << "Scheduling first sent to a random time\n";
+        char msgname[20];
+        sprintf(msgname, "DATA-%ld", numSent);
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(par("delayTime"), msg);
+        emit(transmissionSignal, numSent);
+    }
+}
+
+void Txc1::handleMessage(cMessage *msg){
+
+    //Planning new Message
+    if(getIndex() == 0){
+        numSent++;
+        EV << "Scheduling 1 second after last event\n";
+        char msgname[20];
+        sprintf(msgname, "DATA-%ld", numSent);
+        cMessage *newMsg = new cMessage(msgname);
+        scheduleAt(simTime() + 1, newMsg);
+        emit(transmissionSignal, numSent);
+    }
+
+    // Forwarding Message
+    if(getIndex() == 5){
+        // Message arrived
+        EV << "Message " << msg << " arrived\n";
+        numReceived++;
+        emit(receptionSignal, numReceived);
+        cancelEvent(msg);
+        delete msg; 
+    }
+    else{
+        // Message has to be forwarded
+        forwardMessage(msg);
+        if(getIndex() != 0){
+            numReceived++;
+            emit(receptionSignal, numReceived);
+        }
+        numSent++;
+        emit(transmissionSignal, numSent);
+    }
+}
+
+void Txc1::forwardMessage(cMessage *msg){
+    // For this example, we always reveive in the gate with
+    // a lower number out of the two we have, So we forward 
+    // using our higher-numbered gate
+    int n = gateSize("gate");
+    int k = n - 1;
+    EV << "Forwarding message " << msg << " on gate " << k << "\n";
+    send(msg, "gate$o", k);
+}
