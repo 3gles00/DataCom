69ac334bcff03d2a4d8cedcc94d1a1e7060cd807
3gles00 <haron.obaid66@gmail.com>
2023-09-29 15:08:35 +0200

    solved the reschuduling issue

--- Lab2/P1/multihop/txc1.cc
+++ Lab2/P1/multihop/txc1.cc
@@ -6,47 +6,54 @@ using namespace omnetpp;
 class Txc1 : public cSimpleModule{
     private:
         long msgCounter;
-        // cMessage *event;
+        // cMessage *event = nullptr;
     public:
         virtual ~Txc1();
     protected:
         virtual void initialize();
         virtual void handleMessage(cMessage *msg);
         virtual void forwardMessage(cMessage *msg);
+        // virtual void scheduleMessage(cMessage *msg);
 };
 
 Define_Module(Txc1);
 
 Txc1::~Txc1(){
-    // cancelAndDelete(msg);
-    // delete msgCounter;
+    // cancelAndDelete(event);
+    // delete msg;
 }
 
 void Txc1::initialize(){
-  msgCounter = 0;
-  // Start messaging if I an the first node
-  if(getIndex() == 0){
-    msgCounter++;
-    EV << "Scheduling first sent to a random time\n";
-    char msgname[20];
-    sprintf(msgname, "DATA-%ld", msgCounter);
-    cMessage *msg = new cMessage(msgname);
-    scheduleAt(par("delayTime"), msg);
-  }
+    msgCounter = 0;
+    // event = new cMessage("event");
+    // Start messaging if I an the first node
+    if(getIndex() == 0){
+        msgCounter++;
+        EV << "Scheduling first sent to a random time\n";
+        char msgname[20];
+        sprintf(msgname, "DATA-%ld", msgCounter);
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(par("delayTime"), msg);
+    }
 }
 
 void Txc1::handleMessage(cMessage *msg){
+    //Planning new Message
+    if(getIndex() == 0){
+        // Creating new Message
+        msgCounter++;
+        EV << "Scheduling first sent to a random time\n";
+        char msgname[20];
+        sprintf(msgname, "DATA-%ld", msgCounter);
+        cMessage *newMsg = new cMessage(msgname);
+        scheduleAt(simTime() + 1.0, newMsg);
+    }
+
     if(getIndex() == 5){
         // Message arrived
         EV << "Message " << msg << " arrived\n";
         cancelEvent(msg);
-        delete msg;
-        // Planning new message
-        msgCounter++;
-        char msgname[20];
-        sprintf(msgname, "DATA-%ld", msgCounter);
-        cMessage *msg = new cMessage(msgname);
-        scheduleAt(par("delayTime") + simTime() + 1.0, msg);
+        delete msg; 
     }
     else{
         // Message has to be forwarded
